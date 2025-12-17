01607381b704e078b1cf6a66f9f9ff3e3d9270ab
rfdevuser <rakhisfashions@rakhisfashions.com>
2025-12-17 17:46:30 +0530

    Update RegistrationPage.tsx

--- src/pages/components/RegistrationPage.tsx
+++ src/pages/components/RegistrationPage.tsx
@@ -55,7 +55,7 @@ if(mflag)
                     <p>Payment Details</p>
                      <div className="mb-8">
                       <label htmlFor="studentid" className="mb-3 block text-sm font-medium text-dark dark:text-white">Enter Name</label>
-                      <input type="text" id="studentid" name="reference" value={formData.studentid} onChange={handleChange} placeholder="Enter your name " className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none" autoComplete="reference" required />
+                      <input type="text" id="studentid" name="studentid" value={formData.studentid} onChange={handleChange} placeholder="Enter your name " className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none" autoComplete="reference" required />
                      
                     </div>
                     <div className="mb-8" contentEditable="false">
@@ -97,4 +97,5 @@ if(mflag)
 }
 
 export default RegistrationPage;
+
 
